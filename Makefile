e18ed499bf327328574f5bc039229fda99619119
Jason Zareski <zareskjj@dukes.jmu.edu>
2018-03-22 17:26:36 -0400

    Makefile and testing script made.

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	javac SequentialSampling.java
+clean:
+	rm SequentialSampling.class
---
+++ run.sh
@@ -0,0 +1,3 @@
+echo "Running with Puppy greyscale text file."
+java SequentialSampling < puppy.grey
+echo "Puppy file sampled."
