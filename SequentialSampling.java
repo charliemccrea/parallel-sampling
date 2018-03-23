f7fd86bb6a583ceb3d1d09ea787e596a59d6ce96
Jason Zareski <zareskjj@dukes.jmu.edu>
2018-03-23 14:28:06 -0700

    Serial version now takes the grey file and the original image as parameters.

--- SequentialSampling.java
+++ SequentialSampling.java
@@ -5,24 +5,39 @@ import java.util.Random;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.File;
 
 public class SequentialSampling {
 
-	public static final int RADIUS = 5;
-	public static final int MAX_FAILED_PTS = 1000;
-  public static Random rand = new Random(0);
+	public static final int RADIUS = 3;
+	public static final int MAX_FAILED_PTS = 100000;
   /* DEBUG ATTRS */
   public static final int MAX_ITERATIONS = Integer.MAX_VALUE;//100;
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		// Scanner in = new Scanner(args[1]); //For filename input.
+		//Scanner in = new Scanner(System.in);
+		if (args.length != 2) {
+      System.out.println("Enter a .grey file name and the picture!");
+      return;
+    }
 
-		String dimStr = in.nextLine();
-		System.out.println(dimStr);
-		String[] dim = dimStr.split(" ");
-		int width = Integer.parseInt(dim[0]);
-		int height = Integer.parseInt(dim[1]);
+    Scanner in;
+    int width = 0;
+    int height = 0;
+    String[] dim;
+    String dimStr;
+    try {
+      in = new Scanner(new File(args[0])); //For filename input.
+    	dimStr = in.nextLine();
+		  System.out.println(dimStr);
+		  dim = dimStr.split(" ");
+		  width = Integer.parseInt(dim[0]);
+		  height = Integer.parseInt(dim[1]);
+    } catch (IOException e) {
+      System.out.println("Failed to use the given file.");
+      return;
+    }
+    Random rand = new Random(99);
 		// Create a 2D Array to store
 		boolean[][] pixels = new boolean[width][height];
 		for (int i = 0; i < width; i++) {
@@ -35,12 +50,12 @@ public class SequentialSampling {
 		int failedPts = 0;
 		int itrs = 0;
     while (failedPts < MAX_FAILED_PTS && itrs < MAX_ITERATIONS) {
-			int x = (int) Math.round(rand.nextDouble() * width);
-			int y = (int) Math.round(rand.nextDouble() * height);
-			System.out.print("Iter: "+ itrs +" "+ x + " "  + y + "\n");
+			int x = (int) Math.floor(rand.nextDouble() * width);
+			int y = (int) Math.floor(rand.nextDouble() * height);
+			//System.out.print("Iter: "+ itrs +" "+ x + " "  + y + "\n");
 			Point pt = new Point(x, y);
 			if (darts.contains(pt)) {
-        System.out.println("Point used already! " + failedPts);
+        //System.out.println("Point used already! " + failedPts);
         failedPts++;
 			} else {
 				boolean conflict = false;
@@ -53,50 +68,49 @@ public class SequentialSampling {
 								System.err.println("Conflict!");
 								failedPts++;
 								conflict = true;
-                System.out.println("Failed point! " + failedPts);
+                //System.out.println("Failed point! " + failedPts);
 							}
 						}
 					}
 				}
 				if (!conflict) {
-//					failedPts = 0;
-					// Add point to darts array list, will add to svg image.
-					System.out.println("Added " + pt.getX() + " " + pt.getY());
+					//System.out.println("Added " + pt.getX() + " " + pt.getY());
           darts.add(pt);
           pixels[x][y] = true;
-          try {
-				    outSVG(width,height,darts,"puppy.jpg");
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
         }
 			}
       itrs++;
 		}
+
+    try {
+		  outSVG(width,height,darts,args[0], args[1]);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     System.out.println("Done. Size: " + darts.size());
 	}
 	
-  public static void outSVG(int width, int height, ArrayList<Point> pts, String file) 
+  public static void outSVG(int width, int height, ArrayList<Point> pts, String grey, String picture) 
     throws IOException {
 		// Add a circle tag for each successful point
 		// Output as an SVG file.
+    System.out.println("Picture background: " + picture);
     StringBuilder str = new StringBuilder();
-    str.append("<html>\n<style> svg { background-image: url(\""+file+"\"); }</style>");
+    str.append("<html>\n<style> svg { background-image: url(\""+picture+"\"); }</style>");
     str.append("<svg width=\""+ width +"\" height=\""+height+"\" version=\"1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns=\"http://www.w3.org/2000/svg\">\n");
     //String file will be the filename of the picture to use in SVG file.
     str.append("<defs>\n<pattern id=\"bg_img\" patternUnits=\"userSpaceOnUse\" width=\"" 
         + width + "\" height=\"" + height + "\">\n<image xlink:href=\"" 
-        + file +"\" x=\"0\" y=\"0\" width=\"" + width + "\" height=\"" +height+ "\"/>\n"
+        + grey +"\" x=\"0\" y=\"0\" width=\"" + width + "\" height=\"" +height+ "\"/>\n"
         + "</pattern>\n</defs>\n");
     str.append("<path fill=\"url(#bg_img)\" x=\"0\" y=\"0\" stroke=\"black\" width=\""+width+"\" height=\""+height+"\"/>");
     //str.append("<image x=\"0\" y=\"0\" width=\"" + width + "\" height=\"" + height + "\" xlink:href=\"" + file + "\"/>\n");
     for (Point p : pts) {
       str.append("<circle cx=\"" + p.getX() + "\" cy=\"" + p.getY() +"\" r=\"" + RADIUS + "\" stroke-width=\"1\" fill=\"none\" stroke=\"blue\" />\n");
     }
-      //str.append("<circle cx=\"" + pts.get(0).getX() + "\" cy=\"" + pts.get(0).getY() +"\" r=\"" + RADIUS + "\" stroke-width=\"3\" fill=\"none\" stroke=\"blue\" />\n");
     str.append("</svg>\n</html>\n");
     //Write str.toString() to file, open in browser
-    BufferedWriter write = new BufferedWriter(new FileWriter(file + ".html", false));
+    BufferedWriter write = new BufferedWriter(new FileWriter(grey + ".html", false));
     write.write(str.toString());
     write.close();
     System.err.println("\n***Wrote new SVG!***\n");
--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
-echo "Running with Puppy greyscale text file."
-java SequentialSampling < puppy.grey
+echo "Running with puppy greyscale text file."
+java SequentialSampling puppy.grey puppy.jpg
 echo "Puppy file sampled."
+
+echo "Running with gradient greyscale text file."
+java SequentialSampling gradient.grey gradient.png
+echo "Gradient file sampled."
+
+echo "Running with color ref text file."
+java SequentialSampling ColorValueRef.grey ColorValueRef.png
+echo "color ref file sampled."
