f87d21fee33d00137fa28de728baaade52e739b5
Jason Zareski <zareskjj@dukes.jmu.edu>
2018-03-26 13:44:59 -0400

    Extreme tests added

--- stress_test.sh
+++ stress_test.sh
@@ -12,3 +12,9 @@ java SequentialSampling tests/earth.grey earth.jpg 10 0.10
 echo ""
 echo "=== TEST 5: Earth Image ==="
 java SequentialSampling tests/earth.grey earth.jpg 10 0.05
+echo ""
+echo "=== TEST 5: Earth Image ==="
+java SequentialSampling tests/earth.grey earth.jpg 10 0.025
+echo ""
+echo "=== TEST 5: Earth Image ==="
+java SequentialSampling tests/earth.grey earth.jpg 10 0.00125
