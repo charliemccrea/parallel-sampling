0e7a4cf64c489dd240b5c6135de5eedbe74fa0ce
zareskjj <zareskjj@dukes.jmu.edu>
2018-04-11 16:47:06 -0400

    more changes, need to make the conflict finding method

--- StreamSampling.java
+++ StreamSampling.java
@@ -1,27 +1,47 @@
 import java.util.*;
 import java.util.stream.*;
+import java.util.Random;
 
 public class StreamSampling
 {
   public static void main(String[] args)
   {
+//    Sampling s = new Sampling();
+
     int w = 1000;
     int h = 1000;
     Random r = new Random(0);
     List<Point> pts = new ArrayList<>();
     for (int i = 0; i < 99999; i++)
     {
-      Point p = new Point((int)Math.floor(r.rand(w)), (int)Math.floor(r.rand(h)));
+      Point p = new Point(r.nextInt(w), r.nextInt(h));
       pts.add(p);
     }
-    pts.parallelStream().forEach(p -> 
 
+    pts.parallelStream().forEach(pt -> 
+        {
+          // isConflict(pt);
+          //  Do conflict resolution for every point
+          //  regenerate new list of points if ratio of hits/misses isn't hit
+        }
+    );
   }
-
-  
-
 }
 
-public class SamplingThread
+class Point
+{
+  private int x;
+  private int y;
+  private int colorValue;
+  private int radius;
+  
+  public Point(int x, int y)
+  {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class SamplingThread
 {
 }
