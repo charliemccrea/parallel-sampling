c8c23db925a5adf89e574d025850b2cf1ecfb27e
Jason Zareski <zareskjj@dukes.jmu.edu>
2018-04-25 22:31:54 -0400

    changed tests for p_anisotropic

--- par_run.sh
+++ par_run.sh
@@ -1,11 +1,9 @@
-make
-echo "=== TEST 1: Puppy greyscale file ==="
-java ParallelIsoSampling tests/puppy.grey puppy.jpg 6 0.05
-echo "Puppy file sampled"
-echo ""
-echo "=== TEST 2: Grey Bars file ==="
-java ParallelIsoSampling tests/bars.grey bars.jpg 6 0.05
-echo ""
-echo "=== TEST 3: Earth Image ==="
-java ParallelIsoSampling tests/earth.grey earth.jpg 10 0.25
-echo "Sampled at a ratio of 0.25"
+make parallel_anisotropic
+srun ./p_anisotropic tests/earth.grey 1 > 001.txt &
+srun ./p_anisotropic tests/earth.grey 2 > 002.txt &
+srun ./p_anisotropic tests/earth.grey 4 > 004.txt &
+srun ./p_anisotropic tests/earth.grey 8 > 008.txt &
+srun ./p_anisotropic tests/earth.grey 16 > 016.txt &
+srun ./p_anisotropic tests/earth.grey 32 > 032.txt &
+srun ./p_anisotropic tests/earth.grey 64 > 064.txt &
+srun ./p_anisotropic tests/earth.grey 128 > 128.txt &
